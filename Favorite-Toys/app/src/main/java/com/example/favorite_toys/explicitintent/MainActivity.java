58ea46dfba97286e288acb76c2ac907210d9c0c7
klasing <klasing1959@gmail.com>
2017-11-12 13:47:25 +0100

    12-11-2017 13:46 T04b-02

--- Favorite-Toys/app/src/main/java/com/example/favorite_toys/explicitintent/MainActivity.java
+++ Favorite-Toys/app/src/main/java/com/example/favorite_toys/explicitintent/MainActivity.java
@@ -1,48 +1,48 @@
-package com.example.favorite_toys.explicitintent;
-
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-
-import com.example.favorite_toys.R;
-
-public class MainActivity extends AppCompatActivity {
-
-    private EditText mNameEntry;
-    private Button mDoSomethingCoolButton;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mDoSomethingCoolButton = (Button) findViewById(R.id.b_do_something_cool);
-        mNameEntry = (EditText) findViewById(R.id.et_text_entry);
-
-        mDoSomethingCoolButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                String textEntered = mNameEntry.getText().toString();
-
-                Context context = MainActivity.this;
-
-                Class destinationActivity = ChildActivity.class;
-
-                Intent startChildActivityIntent = new Intent(context, destinationActivity);
-
-//                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
-//                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-
-                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
-
-                startActivity(startChildActivityIntent);
-            }
-        });
-    }
-}
+//package com.example.favorite_toys.explicitintent;
+//
+//import android.content.Context;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.widget.Button;
+//import android.widget.EditText;
+//
+//import com.example.favorite_toys.R;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    private EditText mNameEntry;
+//    private Button mDoSomethingCoolButton;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mDoSomethingCoolButton = (Button) findViewById(R.id.b_do_something_cool);
+//        mNameEntry = (EditText) findViewById(R.id.et_text_entry);
+//
+//        mDoSomethingCoolButton.setOnClickListener(new OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                String textEntered = mNameEntry.getText().toString();
+//
+//                Context context = MainActivity.this;
+//
+//                Class destinationActivity = ChildActivity.class;
+//
+//                Intent startChildActivityIntent = new Intent(context, destinationActivity);
+//
+////                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
+////                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+//
+//                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
+//
+//                startActivity(startChildActivityIntent);
+//            }
+//        });
+//    }
+//}
---
+++ Favorite-Toys/app/src/main/java/com/example/favorite_toys/implicitintents/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.favorite_toys.implicitintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.favorite_toys.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickOpenWebpageButton(View v) {
+        String urlAsString = "https://www.udacity.com";
+
+        openWebPage(urlAsString);
+    }
+
+    public void onClickOpenAddressButton(View v) {
+        //Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+
+        String addressString = "1600 Amphitheatre Parkway, CA";
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+                .path("0,0")
+                .query(addressString);
+        Uri addressUri = builder.build();
+
+        showMap(addressUri);
+    }
+
+    public void onClickShareTextButton(View v) {
+        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+    }
+
+    public void createYourOwn(View v) {
+        Toast.makeText(this,
+                "TODO: Create Your Own Implicit Intent",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+       intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
