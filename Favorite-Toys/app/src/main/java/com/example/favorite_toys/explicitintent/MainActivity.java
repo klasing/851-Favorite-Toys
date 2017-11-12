badc61c44f6e9f94ce5b9a4d6fa24eebe2e655dc
klasing <klasing1959@gmail.com>
2017-11-12 10:46:00 +0100

    12-11-2017 10:45 T04a-02

--- Favorite-Toys/app/src/main/java/com/example/favorite_toys/explicitintent/MainActivity.java
+++ Favorite-Toys/app/src/main/java/com/example/favorite_toys/explicitintent/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.favorite_toys.explicitintent;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.favorite_toys.R;
 
@@ -29,8 +29,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Context context = MainActivity.this;
-                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+
+                Class destinationActivity = ChildActivity.class;
+
+                Intent startChildActivityIntent = new Intent(context, destinationActivity);
+
+//                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
+//                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+
+                startActivity(startChildActivityIntent);
             }
         });
     }
